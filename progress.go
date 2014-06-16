6f65191bde9cdce92afd5275686cb5db1e0c71c1
ernado <ernado@ya.ru>
2014-06-16 18:30:33 +0400

    optimized code

--- progress.go
+++ progress.go
@@ -8,17 +8,14 @@ type Progressor struct {
 	Length   int64
 	Rate     int64
 	Reader   *io.PipeReader
-	Writer   *io.PipeWriter
 	Progress chan float32
 }
 
 func (p *Progressor) Start() {
-	defer p.Writer.Close()
 	var total int64
-	bufLen := p.Length * 1. / p.Rate
+	buffer := make([]byte, p.Length*1./p.Rate)
 	p.Progress <- float32(0)
 	for {
-		buffer := make([]byte, bufLen)
 		read, err := p.Reader.Read(buffer)
 		total += int64(read)
 		p.Progress <- float32(total) / float32(p.Length) * 100
@@ -32,7 +29,7 @@ func (p *Progressor) Start() {
 func Progress(f io.Reader, length int64, rate int64, progress chan float32) io.Reader {
 	progressReader, progressWriter := io.Pipe()
 	reader := io.TeeReader(f, progressWriter)
-	p := Progressor{length, rate, progressReader, progressWriter, progress}
+	p := Progressor{length, rate, progressReader, progress}
 	go p.Start()
 	return reader
 }
