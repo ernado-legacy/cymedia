9032aa17c9c596d5791ac16f0c419ed09641f491
ernado <ernado@ya.ru>
2014-07-26 21:06:36 +0400

    improved tests

--- mediad/query/query_test.go
+++ mediad/query/query_test.go
@@ -1,14 +1,23 @@
 package query
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"github.com/ernado/cymedia/mediad/models"
 	. "github.com/smartystreets/goconvey/convey"
-	// "os"
 	"testing"
 )
 
+func randStr(length int) string {
+	b := make([]byte, length)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
+
 func TestQuery(t *testing.T) {
 	Convey("Memory query", t, func() {
+
 		q := NewMemoryQuery()
 		request := models.Request{}
 		request.Id = "request"
@@ -20,7 +29,7 @@ func TestQuery(t *testing.T) {
 	})
 
 	Convey("Redis query", t, func() {
-		q, err := NewRedisQuery(":6379", "test:cymedia:key")
+		q, err := NewRedisQuery(":6379", fmt.Sprintf("test:cymedia:%s", randStr(20)))
 		So(err, ShouldBeNil)
 		request := models.Request{}
 		request.Id = "request"
