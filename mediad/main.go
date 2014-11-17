64f86e8b82351737cc3fd1d6cde8eb1979c7871f
ernado <ernado@ya.ru>
2014-11-17 17:34:09 +0300

    added storage support

--- mediad/main.go
+++ mediad/main.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"flag"
-	"github.com/ernado/cymedia/mediad/query"
+	"io"
 	"log"
+
+	"github.com/ernado/cymedia/mediad/query"
+	"github.com/ernado/selectel/storage"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var (
-	weedUrl   = flag.String("weed", "http://localhost:9333", "Weed master url")
-	redisHost = flag.String("redis.addr", ":6379", "Redis server address")
-	redisKey  = flag.String("redis.key", "cymedia:query", "Redis query key")
+	weedUrl           = flag.String("weed", "http://localhost:9333", "Weed master url")
+	redisHost         = flag.String("redis.addr", ":6379", "Redis server address")
+	redisKey          = flag.String("redis.key", "cymedia:query", "Redis query key")
+	selectel          bool
+	selectelContainer = flag.String("selectel.container", "", "Selectel container")
 )
+
+func init() {
+	flag.BoolVar(&selectel, "selectel", false, "Use selectel storage")
+}
+
+type selectelAdapter struct {
+	api storage.ContainerAPI
+}
+
+func (s selectelAdapter) GetUrl(name string) (string, error) {
+	return s.api.URL(name), nil
+}
+
+func (s selectelAdapter) URL(name string) (string, error) {
+	return s.GetUrl(name)
+}
+
+func (s selectelAdapter) Upload(reader io.Reader, t, format string) (fid string, purl string, size int64, err error) {
+	fid = bson.NewObjectId().Hex()
+	err = s.api.Upload(reader, fid, t+"/"+format)
+	purl = s.api.URL(fid)
+	return
+}
 
 func main() {
 	flag.Parse()
 	log.Println("connecting")
-	server, err := query.NewRedisServer(*weedUrl, *redisHost, *redisKey)
+	var (
+		server     query.QueryServer
+		err        error
+		selStorage storage.API
+	)
+	if selectel {
+		selStorage, err = storage.NewEnv()
+		if err != nil {
+			log.Fatal(err)
+		}
+		container := selStorage.Container(*selectelContainer)
+		server, err = query.NewRedisSelectelServer(selectelAdapter{container}, *redisHost, *redisKey)
+	} else {
+		server, err = query.NewRedisServer(*weedUrl, *redisHost, *redisKey)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
--- mediad/query/server.go
+++ mediad/query/server.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/ernado/cymedia/mediad/conventer"
-	"github.com/ernado/cymedia/mediad/models"
-	"github.com/garyburd/redigo/redis"
-	"github.com/ginuerzh/weedo"
 	"io"
 	"log"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/ernado/cymedia/mediad/conventer"
+	"github.com/ernado/cymedia/mediad/models"
+	"github.com/ernado/weed"
+	"github.com/garyburd/redigo/redis"
 )
 
 var (
@@ -21,11 +22,16 @@ var (
 	ErrorCritical = errors.New("critical error")
 )
 
+type StorageAdapter interface {
+	GetUrl(fid string) (url string, err error)
+	Upload(reader io.Reader, t, format string) (fid string, purl string, size int64, err error)
+}
+
 type Server struct {
-	conn  redis.Conn
-	weed  *weedo.Client
-	video conventer.Conventer
-	query Query
+	conn    redis.Conn
+	storage StorageAdapter
+	video   conventer.Conventer
+	query   Query
 }
 
 func (s *Server) MakeResponce(request models.Request) (err error) {
@@ -69,8 +75,10 @@ func (s *Server) Iteration() error {
 }
 
 func (s *Server) Main() {
-	var err error
-	var sleep time.Duration
+	var (
+		sleep time.Duration
+		err   error
+	)
 	log.Println("started")
 	for {
 		if err = s.Iteration(); err != nil {
@@ -88,7 +96,7 @@ func (s *Server) Main() {
 
 func NewTestServer() (QueryServer, Query) {
 	s := new(Server)
-	s.weed = weedo.NewClient("http://localhost:9333")
+	s.storage = weed.NewAdapter("http://localhost:9333")
 	s.query = NewMemoryQuery()
 	s.video = new(conventer.VideoConventer)
 	return s, s.query
@@ -96,7 +104,16 @@ func NewTestServer() (QueryServer, Query) {
 
 func NewRedisServer(weedUrl, redisHost, redisKey string) (server QueryServer, err error) {
 	s := new(Server)
-	s.weed = weedo.NewClient(weedUrl)
+	s.storage = weed.NewAdapter(weedUrl)
+	s.video = new(conventer.VideoConventer)
+	s.query, err = NewRedisQuery(redisHost, redisKey)
+	s.conn, err = redis.Dial("tcp", redisHost)
+	return s, err
+}
+
+func NewRedisSelectelServer(storage StorageAdapter, redisHost, redisKey string) (server QueryServer, err error) {
+	s := new(Server)
+	s.storage = storage
 	s.video = new(conventer.VideoConventer)
 	s.query, err = NewRedisQuery(redisHost, redisKey)
 	s.conn, err = redis.Dial("tcp", redisHost)
@@ -104,7 +121,7 @@ func NewRedisServer(weedUrl, redisHost, redisKey string) (server QueryServer, er
 }
 
 func (s *Server) Convert(req models.Request) (output io.ReadCloser, err error) {
-	url, _, err := s.weed.GetUrl(req.File)
+	url, err := s.storage.GetUrl(req.File)
 	if err != nil {
 		return
 	}
@@ -141,7 +158,7 @@ func (s *Server) Process(request models.Request) (response models.Responce, err 
 	if err != nil {
 		return
 	}
-	fid, _, err := s.weed.AssignUpload(fmt.Sprintf("file.%s", options.Extension()), options.Mime(), output)
+	fid, _, _, err := s.storage.Upload(output, fmt.Sprintf("file.%s", options.Extension()), options.Mime())
 	if err != nil {
 		return
 	}
